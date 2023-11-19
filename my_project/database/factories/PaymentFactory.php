39f992790cfc5cdc76fa2ff780748a2c3a0cea9d
zhenya1907 <e.group550@gmail.com>
2023-11-19 18:45:08 +0400

    docker + tables + data

---
+++ my_project/database/factories/PaymentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payment>
+ */
+class PaymentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Payment::class;
+
+    public function definition(): array
+    {
+        return [
+            'amount' => fake()->randomFloat(2, 10, 1000),
+            'provider' => fake()->word,
+            'user_id' => fake()->numberBetween(1, 5),
+        ];
+    }
+}
---
+++ my_project/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(5)->create();
+        Payment::factory(10)->create();
+
+    }
+}
